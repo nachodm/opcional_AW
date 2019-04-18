ce308f333c3dd4697b54e292fbf18d0db5fd1d65
Nacho Domingo <ignacdm10@outlook.com>
2019-04-18 20:00:12 +0200

    Add files via upload
    Falta por cambiar las funciones insertTask y getAllTasks (pueden recibir multiples tags y ahora mismo no funcionan así)

--- Practica_guiada_3/Public/js/DAOTasks.js
+++ Practica_guiada_3/Public/js/DAOTasks.js
@@ -3,23 +3,30 @@
 class DAOTasks{
 
     /**
-     * 
+     * Pool de conexiones a la base de datos
      * @param {*} pool 
      */
     constructor(pool){
         this.pool = pool;
     }
 
+    /**
+     * Devuelve todas las tareas del usuario especificado por parámetro.
+     * 
+     * @param {string} email Identificador del usuario.
+     * @param {function} callback Función callback.
+     */
     getAllTasks(email, callback){
-        this.pool.getConnection((err, conn)=>{
-            if(err){
-                callback("Error de conexion a la BBDD");
+        this.pool.getConnection((err, conn) => {
+            if (err) {
+                callback("Error de conexión a la base de datos", undefined);
             }
-            conn.query("SELECT * FROM task t JOIN tag ta ON t.id = ta.taskId where t.user = ?",[email],
-            (err, rows)=>{
+            conn.query("SELECT * FROM task t JOIN tag ta ON t.id = ta.taskId where t.user = ?",
+            [email],
+            (err, rows) => {
                 conn.release();
-                if(err){
-                    callback("Error de acceso a la BBDD");
+                if (err) {
+                    callback("Error de acceso a la base de datos", undefined);
                 }
                 var arrayResponse = [];
                 
@@ -32,7 +39,7 @@ class DAOTasks{
                     };
                     arrayResponse.push(object);
                 });
-                callback(undefined, arrayResponse);
+                callback(null, arrayResponse);
                 
             });
         })
@@ -41,22 +48,21 @@ class DAOTasks{
     insertTask(email, task, callback){
         this.pool.getConnection((err, conn)=>{
             if(err){
-                callback("Error de conexion a la BBDD");
+                callback("Error de conexión a la base de datos");
             }
             conn.query("INSERT INTO task (user, text, done) VALUES (?,?,?)", 
             [email, task.text, task.done], (err, row)=>{
                
                 if(err){
-                    callback("Error de acceso a la BBDD");
+                    callback("Error de acceso a la base de datos");
                 }
                 conn.query("INSERT INTO tag (taskId, tag) VALUES (?,?)", [row.insertId, task.tag],
                 (err) =>{
                     conn.release();
-                    if(err){
-                        
-                        callback(undefined, false);
+                    if (err) {
+                        callback("Error de acceso a la base de datos");
                     }
-                    else callback(undefined, true);
+                    else callback(null);
                 });
             });
         });
@@ -65,14 +71,14 @@ class DAOTasks{
     markTaskDone(idTask, callback){
         this.pool.getConnection((err, conn)=>{
             if(err){
-                callback("Error de conexion a la BBDD");
+                callback("Error de conexión a la base de datos");
             }
             conn.query("UPDATE task SET done = true WHERE id = ?",[idTask], (err) =>{
                 conn.release();
                 if(err){
-                    callback("Error de acceso a la BBDD");
+                    callback("Error de acceso a la base de datos");
                 }
-                else callback(undefined, true);
+                else callback(null);
             })
         })
     }
@@ -80,15 +86,15 @@ class DAOTasks{
     deleteCompleted(email, callback){
         this.pool.getConnection((err, conn)=>{
             if(err){
-                callback("Error de conexion a la BBDD", false);
+                callback("Error de conexión a la base de datos");
             }
             conn.query("DELETE FROM task WHERE user = ? AND done = 1", [email], (err)=>{
                 conn.release();
                 if(err){
-                    callback("Error de acceso a la BBDD", false);
+                    callback("Error de acceso a la base de datos");
                 }
                 else{
-                    callback(undefined, true);
+                    callback(null);
                 }
             })
         })
--- Practica_guiada_3/Public/js/DAOUsers.js
+++ Practica_guiada_3/Public/js/DAOUsers.js
@@ -3,46 +3,65 @@
 class DAOUsers{
 
     /**
+     * Inicializa el DAO de usuarios.
      * 
-     * @param {*} pool 
+     * @param {Pool} pool Pool de conexiones MySQL. Todas las operaciones
+     *                    sobre la BD se realizarán sobre este pool.
      */
     constructor(pool){
         this.pool = pool;
     }
 
-
+    /**
+     * Determina si un determinado usuario aparece en la BD con la contraseña
+     * pasada como parámetro.
+     * 
+     * @param {string} email Identificador del usuario a buscar
+     * @param {string} password Contraseña a comprobar
+     * @param {function} callback Función que recibirá el objeto error y el resultado
+     */
     isUserCorrect(email, password, callback){
-       
-        this.pool.getConnection((err, conn)=>{
-            if(err){
-                callback("Error de conexion con la BBDD", undefined);
+        this.pool.getConnection((err, conn) => {
+            if (err) {
+                callback("Error de conexión con la BBDD", undefined);
             }
       
             conn.query("SELECT email, password FROM user WHERE email = ? AND password = ?",
-            [email, password], (err, row)=>{
-                
+            [email, password], 
+            (err, rows)=>{
                 conn.release();
                 if(err){
                     callback("Error de acceso a la BBDD", undefined);
                 }
-                if (row.length > 0) callback(null, true);
-                else callback(null , false);
+                if (rows.length > 0) callback(null, true);
+                else callback(null, false);
             });
         });
     }
 
+    /**
+     * Obtiene el nombre del fichero que contiene la imagen de perfil de un usuario.
+     * 
+     * 
+     * @param {string} email Identificador del usuario cuya imagen se quiere obtener
+     * @param {function} callback Función que recibirá el objeto error y el resultado
+     */
     getUserImageName(email, callback){
-        this.pool.getConnection((err, conn)=>{
-            if(err){
-                callback("Error de conexion con la BBDD", undefined);
+        this.pool.getConnection((err, conn) => {
+            if (err) {
+                callback("Error de conexión con la BBDD", undefined);
             }
-            conn.query("SELECT img FROM user WHERE email = ?", [email], (err, row)=>{
+            conn.query("SELECT img FROM user WHERE email = ?",
+            [email],
+            (err, row)=>{
                 conn.release();
-                if(err){
+                if (err) {
                     callback("Error de acceso a la BBDD");
                 }
-                else {
-                    callback(undefined, row[0]);
+                if (row.length === 0) {
+                    callback(null, undefined);
+                } else {
+                    callback(null, row[0].img);
                 }
             });
         });
