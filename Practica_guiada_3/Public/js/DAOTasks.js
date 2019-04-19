694b1fc582d0c5384d89dd35c36725a5b15537fd
Nacho Domingo <ignacdm10@outlook.com>
2019-04-19 19:35:37 +0200

    Update DAOTasks.js

--- Practica_guiada_3/Public/js/DAOTasks.js
+++ Practica_guiada_3/Public/js/DAOTasks.js
@@ -21,49 +21,65 @@ class DAOTasks{
             if (err) {
                 callback("Error de conexión a la base de datos", undefined);
             }
-            conn.query("SELECT * FROM task t JOIN tag ta ON t.id = ta.taskId where t.user = ?",
+            conn.query("SELECT * FROM task t LEFT JOIN tag ta ON t.id = ta.taskId where t.user = ? ORDER BY id ASC;",
             [email],
             (err, rows) => {
                 conn.release();
                 if (err) {
                     callback("Error de acceso a la base de datos", undefined);
                 }
-                var arrayResponse = [];
-                
-                rows.forEach(p => {
-                    var object = {
-                        id : p.id,
-                        text: p.text,
-                        done: p.done,
-                        tag: p.tag
-                    };
-                    arrayResponse.push(object);
-                });
-                callback(null, arrayResponse);
-                
+
+                let tasks = [], id, text, done, tags = [], add = false;
+
+                rows.forEach(row => {
+                    if (id !== row.id) {
+                        if (add) {
+                            tasks.push({ id: id, text: text, done: done, tags: tags });
+                            tags = [];
+                        }
+                        id = row.id;
+                        text = row.text;
+                        done = row.done;
+                        add = true;
+                    }
+                    if (row.tag !== null){
+                        tags.push(row.tag);
+                    }
+                })
+                /**Último elemento */
+                if (rows.length > 0) { 
+                    tasks.push({ id: id, text: text, done: done, tags: tags }); 
+                }
+                callback(null, tasks);
             });
         })
     }
 
     insertTask(email, task, callback){
+        let separation = task.text.split(" @");
+
         this.pool.getConnection((err, conn)=>{
             if(err){
                 callback("Error de conexión a la base de datos");
             }
             conn.query("INSERT INTO task (user, text, done) VALUES (?,?,?)", 
-            [email, task.text, task.done], (err, row)=>{
+            [email, separation[0], task.done], (err, row)=>{
                
                 if(err){
                     callback("Error de acceso a la base de datos");
                 }
-                conn.query("INSERT INTO tag (taskId, tag) VALUES (?,?)", [row.insertId, task.tag],
-                (err) =>{
-                    conn.release();
-                    if (err) {
-                        callback("Error de acceso a la base de datos");
+                if (separation.length > 1) {
+                    for (let i = 1; i < separation.length; i++) {
+                        conn.query("INSERT INTO tag (taskId, tag) VALUES (?,?)", [row.insertId, separation[i]],
+                        (err) =>{
+                            if (err) {
+                                callback("Error de acceso a la base de datos");
+                            }
+                        });
                     }
-                    else callback(null);
-                });
+                    conn.release();
+                }
+                callback(null);
             });
         });
     }
